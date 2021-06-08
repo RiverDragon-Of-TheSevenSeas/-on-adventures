e53ec666fcdc01c470bb463dd5d656d427216b79
eld1d <eldiarioded1d@hotmail.com>
2021-06-08 16:51:28 -0500

    intento de movimiento basico
    se añadió a nhon y un script de movimiento

---
+++ Ñon Adventures/Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    float horizontalMove = 0f;
+    public float speed = 0f;
+
+    public CharacterController2D controller;    
+    
+    // This script is meant to check player inputs for movement.
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+    }
+
+    void FixedUpdate()
+    {
+        //Movement
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
---
+++ Ñon Adventures/Assets/movement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    // This script is in charge of making the main character move.
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
